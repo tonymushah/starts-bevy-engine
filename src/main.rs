eeeeee94ac2f8cf54b9ecd3ab999a9015a7fa4d3
tonymushah <tonyramanantsalama@gmail.com>
2023-10-29 17:07:08 +0300

    Finished `Hello world!`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn hello_world() {
+    println!("Hello world!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Tony".to_string())));
+    commands.spawn((Person, Name("Akari".to_string())));
+    commands.spawn((Person, Name("Tsukasa".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>){
+    for name in &query {
+        println!("hello {}!", name.0)
+    }
+}
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
 }
