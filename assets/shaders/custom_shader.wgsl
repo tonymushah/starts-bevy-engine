c724e99347959cc65982bc2034b81c845829b18b
tonymushah <tonyramanantsalama@gmail.com>
2025-07-14 00:08:21 +0300

    my first shader

---
+++ assets/shaders/custom_shader.wgsl
@@ -0,0 +1,10 @@
+@group(2) @binding(0)
+var<uniform> time: f32;
+
+@fragment
+fn fragment(@location(0) uv: vec2<f32>) -> @location(0) vec4<f32> {
+    let r = 0.5 + 0.5 * sin(time + uv.x * 10.0);
+    let g = 0.5 + 0.5 * sin(time + uv.y * 10.0);
+    let b = 0.5 + 0.5 * sin(time);
+    return vec4<f32>(r, g, b, .0);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,81 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::render_resource::AsBindGroup};
+
+#[derive(Debug, AsBindGroup, TypePath, Asset, Clone)]
+struct MyCustomShader {
+    #[uniform(0)]
+    time: f32,
+}
+
+impl Material for MyCustomShader {
+    fn fragment_shader() -> bevy::render::render_resource::ShaderRef {
+        "shaders/custom_shader.wgsl".into()
+    }
+    fn alpha_mode(&self) -> AlphaMode {
+        AlphaMode::AlphaToCoverage
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut my_custom_shaders: ResMut<Assets<MyCustomShader>>,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(my_custom_shaders.add(MyCustomShader { time: 0.0 })),
+    ));
+
+    commands.spawn((
+        PointLight::default(),
+        Transform {
+            translation: Vec3 {
+                x: 4.0,
+                y: 3.0,
+                z: 2.0,
+            },
+            ..Default::default()
+        },
+    ));
+
+    commands.spawn((
+        Camera3d::default(),
+        Transform {
+            translation: Vec3 {
+                x: 3.0,
+                y: 4.0,
+                z: -2.0,
+            },
+            ..Default::default()
+        }
+        .looking_at(Vec3::default(), Vec3::Y),
+    ));
+}
+
+fn update_time_texture(
+    mut my_custom_shaders: ResMut<Assets<MyCustomShader>>,
+    time: Res<Time>,
+    has_materials: Query<&MeshMaterial3d<MyCustomShader>>,
+) {
+    for has_material in has_materials {
+        if let Some(material) = my_custom_shaders.get_mut(has_material) {
+            material.time = time.elapsed_secs();
+        }
+    }
+}
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            watch_for_changes_override: if cfg!(debug_assertions) && cfg!(feature = "asset_reload")
+            {
+                Some(true)
+            } else {
+                None
+            },
+            ..Default::default()
+        }))
+        .add_plugins(MaterialPlugin::<MyCustomShader>::default())
+        .add_systems(Startup, setup)
+        .add_systems(Update, update_time_texture)
+        .run();
 }
